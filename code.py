3a9b39a817675db18b9a4330a9013f1d8f9e01a4
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-07-28 23:28:23 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,59 +1,125 @@
-
-from tkinter import *
-from tkinter import ttk
-from random import *
-# Create Object
-root = Tk()
-# Set geometry
-root.geometry("500x500")
-root.title("Rock-Paper-Scissors-Game")
-# List of players
-list = ["rock","paper","scissors"]
-choose_number = randint(0,2)
-print(choose_number) # For testing if it works
-label = Label(root,text="select your choice",width = 20,height=4,font=("calibri",15))
-label.pack()
-def spin():
-    choose_number = randint(0,2)
-    label.config(text=list[choose_number])
-    if user_select.get() == "Rock":
-        user_select_value = 0
-        print(user_select_value)
-    elif user_select.get() == "Paper":
-        user_select_value = 1
-        print(user_select_value)
-    elif user_select.get() == "Scissors":
-        user_select_value = 2
-        print(user_select_value)
-    if user_select_value == 0:
-        if choose_number == 0:
-            wl_label.config(text="Tie! - "+" Computer:Bad luck")
-        elif choose_number == 1:
-            wl_label.config(text="YOU Loose - "+" Computer: I am better ")
-        elif choose_number == 2 :
-            wl_label.config(text="YOU Won - "+" Computer: You won by luck")
-    elif user_select_value == 1:
-        if choose_number == 1:
-            wl_label.config(text="Tie! - "+" Computer: Nice game")
-        elif choose_number == 0:
-            wl_label.config(text="YOU Won - "+" Computer: Shit how you are better")
-        elif choose_number == 2 :
-            wl_label.config(text="YOU Loose - "+" Computer: booo")
-    elif user_select_value == 2:
-        if choose_number == 2:
-            wl_label.config(text="Tie!")
-        elif choose_number == 0:
-            wl_label.config(text="YOU Loose - "+" Computer: I am playing this game since i was born")
-        elif choose_number == 1 :
-            wl_label.config(text="YOU Won")
-# Adding dropdown box for Rock,Paper,Scissors
-user_select = ttk.Combobox(root,value=["Rock","Paper","Scissors"])
-user_select.current(0)
-user_select.pack()
-# Add Labels,Button
-wl_label = Label(root,text="",font=("arial",10),width=50,height=4)
-wl_label.pack()
-button = Button(root,text="Spin",font=("bell mt",10),command=spin)
-button.pack()
-root.mainloop()
-
+import random
+print("welcome to scissors,papers and rock game:)")
+user=input("what is user name: ")
+password=input("what is password: ")
+if password=="shivz@2004":
+    print("Entered to Game!")
+else:
+    while True :
+        a=input("Tell me the correct password! ")
+        if a=="shivz@2004":
+            print("Entered to Game!")
+            break
+compscore=0
+userscore=0
+print("hello %s you are in the hell now :( " %user)
+r1=input("Round 1. what is your call(scissors,papers,rock) : ")
+comp="scissors","papers","rock"
+comp1=random.choice(comp)
+print("Computers Call: %s " %comp1)
+if r1=="rock" and comp1=="papers":
+    print("you loose:( computer wins")
+    compscore+=1
+elif r1=="rock" and comp1=="scissors":
+    print("yay! you win:) ")
+    userscore+=1
+elif r1=="rock" and comp1=="rock":
+    print("Draw both will get 1 points ")
+    compscore+=1
+    userscore+=1
+elif r1=="papers" and comp1=="papers":
+    print("Draw Both will get 1 points")
+    compscore+=1
+    userscore+=1
+elif r1=="papers" and comp1=="scissors":
+    print("you loose:( computer wins")
+    compscore+=1
+elif r1=="papers" and comp1=="rock":
+    print("yay! you win:) ")
+    userscore+=1
+elif r1=="scissors" and comp1=="scissors":
+    print("Draw both will get 1 points ")
+    compscore+=1
+    userscore+=1
+elif r1=="scissors" and comp1=="papers":
+    print("yay! you win:) ")
+    userscore+=1
+elif r1=="scissors" and comp1=="rock":
+    print("you loose:( computer wins")
+    compscore+=1
+r2=input("Round 2. what is your call(scissors,papers,rock) : ")
+comp="scissors","papers","rock"
+comp1=random.choice(comp)
+print("Computers Call: %s " %comp1)
+if r2=="rock" and comp1=="papers":
+    print("you loose:( computer wins")
+    compscore+=1
+elif r2=="rock" and comp1=="scissors":
+    print("yay! you win:) ")
+    userscore+=1
+elif r2=="rock" and comp1=="rock":
+    print("Draw both will get 1 points ")
+    compscore+=1
+    userscore+=1
+elif r2=="papers" and comp1=="papers":
+    print("Draw Both will get 1 points")
+    compscore+=1
+    userscore+=1
+elif r2=="papers" and comp1=="scissors":
+    print("you loose:( computer wins")
+    compscore+=1
+elif r2=="papers" and comp1=="rock":
+    print("yay! you win:) ")
+    userscore+=1
+elif r2=="scissors" and comp1=="scissors":
+    print("Draw both will get 1 points ")
+    compscore+=1
+    userscore+=1
+elif r2=="scissors" and comp1=="papers":
+    print("yay! you win:) ")
+    userscore+=1
+elif r2=="scissors" and comp1=="rock":
+    print("you loose:( computer wins")
+    compscore+=1
+r3=input("Round 3. what is your call(scissors,papers,rock) : ")
+comp="scissors","papers","rock"
+comp1=random.choice(comp)
+print("Computers Call: %s " %comp1)
+if r3=="rock" and comp1=="papers":
+    print("you loose:( computer wins")
+    compscore+=1
+elif r3=="rock" and comp1=="scissors":
+    print("yay! you win:) ")
+    userscore+=1
+elif r3=="rock" and comp1=="rock":
+    print("Draw both will get 1 points ")
+    compscore+=1
+    userscore+=1
+elif r3=="papers" and comp1=="papers":
+    print("Draw Both will get 1 points")
+    compscore+=1
+    userscore+=1
+elif r3=="papers" and comp1=="scissors":
+    print("you loose:( computer wins")
+    compscore+=1
+elif r3=="papers" and comp1=="rock":
+    print("yay! you win:) ")
+    userscore+=1
+elif r3=="scissors" and comp1=="scissors":
+    print("Draw both will get 1 points ")
+    compscore+=1
+    userscore+=1
+elif r3=="scissors" and comp1=="papers":
+    print("yay! you win:) ")
+    userscore+=1
+elif r3=="scissors" and comp1=="rock":
+    print("you loose:( computer wins")
+    compscore+=1
+print("computers total score is %s"%compscore)
+print("user total score is %s" %userscore)
+if userscore>compscore:
+    print("yay you win the game champion")
+elif compscore>userscore:
+    print(" you loose! the game better luck next time")
+elif compscore==userscore:
+    print("Draw! play again")
